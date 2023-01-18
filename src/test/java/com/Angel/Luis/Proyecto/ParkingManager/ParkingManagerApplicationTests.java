394c6ca956e47461ac81229268a25aeebe8277ef
Ajolote41 <predstone41@gmail.com>
2023-01-18 20:16:52 +0100

    Creación del proyecto

---
+++ src/test/java/com/Angel/Luis/Proyecto/ParkingManager/ParkingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Angel.Luis.Proyecto.ParkingManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
