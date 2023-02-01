621c909181ab1ec919b7b22b928efff5faf1d8ea
Angelinero48 <116581341+Angelinero48@users.noreply.github.com>
2023-02-01 20:20:19 +0100

    Proyecto

---
+++ src/main/java/com/Angel/Luis/Proyecto/ParkingManager/user/UserController.java
@@ -0,0 +1,19 @@
+package com.Angel.Luis.Proyecto.ParkingManager.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/users")
+    public String displayUsers(Model model) {
+        model.addAttribute("users", repository.findAll());
+        return "userlist";
+
+    }
+}
